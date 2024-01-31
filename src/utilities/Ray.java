1e189fa92b8217587ae6adeb4147409eb0daf369
AnsonSavage <ansonsavage@gmail.com>
2024-01-30 21:01:27 -0700

    Fixed the black hole error by lowering the originOffset

--- src/utilities/Ray.java
+++ src/utilities/Ray.java
@@ -3,7 +3,7 @@ package utilities;
 public class Ray {
     private Vector3 origin;
     private Vector3 direction;
-    private double originOffset = 0.0001;
+    private double originOffset = 0.00000001;
 
     public Ray(Vector3 origin, Vector3 direction) {
         this.origin = origin;
